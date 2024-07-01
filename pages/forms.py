58a062acacd30fb466235d6d4877fa6bdd914e7d
Aravind Gunasekaran <aravindusa@yahoo.com>
2024-07-01 18:24:34 -0400

    git ignor

--- pages/forms.py
+++ pages/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     email = forms.EmailField(label='Your email')
+    number = forms.CharField(label='Your phone number', max_length=15)
     message = forms.CharField(widget=forms.Textarea)
